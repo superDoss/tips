b6c18107d5e058f8a8e2b3f748f14b763d236b0b
davidHeymann <heymann.david@gmail.com>
2019-03-13 15:55:09 +0200

    add rate api

--- api/TipsController.cs
+++ api/TipsController.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 using System.Linq;
 using System.Threading.Tasks;
 using Tips.Models;
 using Tips.Database;
+using tips.api;
 
 namespace Tips.api
 {
@@ -13,15 +15,35 @@ namespace Tips.api
     public class TipsController : ControllerBase
     {
         private readonly TipsContext _context;
-         public TipsController(TipsContext context)
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public TipsController(TipsContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
+
         [HttpGet]
+        //[Route("api/tips/")]
         public async Task<ActionResult<IEnumerable<Tip>>> GetTips()
         {
             return await _context.Tips.OrderByDescending(x => x.CreateDate).Take(5).ToListAsync();
+        }
+
+        [HttpPost("rate")]
+        //[Route("api/tips/rate")]
+        public async Task<ActionResult> RateTips(TipRateReq tipRateReq)
+        {
+            var sysUserId = _userManager.GetUserId(User);
+            TipRating tipRate = new TipRating();
+            tipRate.TipId = tipRateReq.TipId;
+            tipRate.UserId = _context.Users.FirstOrDefault(u => u.SysUserId == sysUserId).Id;
+            tipRate.RateValue = tipRateReq.RateValue;
+            _context.Add(tipRate);
+            await _context.SaveChangesAsync();
+
+            return Ok();
         }
 
         [HttpGet("search")]
